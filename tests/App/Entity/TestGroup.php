6d0b05b9f9a5a9f9095c82101d52adf12672a86b
core23 <mail@core23.de>
2025-10-20 20:38:06 +0200

    Add support for nucleos/user-bundle 4

--- tests/App/Entity/TestGroup.php
+++ tests/App/Entity/TestGroup.php
@@ -13,11 +13,19 @@ namespace Nucleos\ProfileBundle\Tests\App\Entity;
 
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Nucleos\UserBundle\Entity\BaseGroup;
+use Nucleos\UserBundle\Entity\BaseUser;
 use Nucleos\UserBundle\Model\Group;
+
+if (class_exists(BaseUser::class)) {
+    abstract class InternalTestGroup extends BaseGroup {}
+} else {
+    abstract class InternalTestGroup extends Group {}
+}
 
 #[ORM\Entity]
 #[ORM\Table(name: 'user__group')]
-class TestGroup extends Group
+class TestGroup extends InternalTestGroup
 {
     #[ORM\Id]
     #[ORM\Column(type: Types::INTEGER)]
